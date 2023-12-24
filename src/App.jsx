228dec34ed9444f33f7af125b4ad8026ec760ab5
asher777777 <ovt5771@gmail.com>
2023-12-24 12:42:28 +0200

    chack

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/signin"
+import SignUp from "./pages/signup"
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes> 
+<Route path="/" element={<Home />} /> 
+<Route path="/SignIn" element={<SignIn />} /> 
+<Route path="/SignUp" element={<SignUp />} />
+
+
+
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
